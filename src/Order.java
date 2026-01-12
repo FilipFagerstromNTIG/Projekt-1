c46b73ed4746dbfa44df965fd695ec8fc25f77cd
Filip Fagerström <filip.fagerstrom@elev.ga.ntig.se>
2026-01-12 11:04:30 +0100

    Add Order class with properties and methods for order management

---
+++ src/Order.java
@@ -0,0 +1,23 @@
+public class Order {
+    private int id;
+    private String customerName;
+    private Building building;
+
+    public Order(int id, String name, Building building) {
+        this.id = id;
+        this.customerName = name;
+        this.building = building;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return building.getPrice();
+    }
+
+    public string toString() {
+        return "Order ID: " + id + ", Customer: " + customerName + ", Building Type: " + building.getType() + ", Price: " + getPrice();
+    }
+}
